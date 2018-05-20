d838c865f24c6edfbe8f46f87641ab00afa6960f
lesleyfon <lesleybanadzem2017@gmail.com>
2018-05-19 18:47:11 -0700

    got the game working, scores updating displaying etc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,101 @@
+// create a hash that stores all the global variables
+var data = {
+    wins:0,
+    losses:0,
+    target: numberGenerator(19, 120),
+    total:0,
+    gems: []
+}
 
-        // creating some variables
-        var wins = 0;
-        var losses = 0;
-        var userChoice = [];
-        var randomComputer = [];
+// create a function that generates a random number between x-y
+function numberGenerator(min, max){
+    return Math.floor(Math.random()*(max-min+1)+1);
+}
 
-        var total = [];
+// assign 4 values to an array
+function assignGemValues(){
+    while (data.gems.length < 5) {
+        var num = numberGenerator(1, 12);
+        if(!data.gems.includes(num)){
+            data.gems.push(num);
+        }
+    }
+}
+// initaial value
+assignGemValues();
 
-        var choice1 = Math.floor(Math.random()*12) +1;             
-        var choice2 = Math.floor(Math.random()*12) +1;             
-        var choice3 = Math.floor(Math.random()*12) +1;             
-        var choice4 = Math.floor(Math.random()*12) +1;             
-        var choiceComp = Math.floor(Math.random()*100) +18; 
-        
-        
-                
-        
 
+function displayHTML(){
+    $("#wins").text(data.wins);
+    $("#losses").text(data.losses);
+    $("#target").text(data.target);
+    $("#total").text(data.total);
+}
+// display initaial value
+displayHTML();
 
-    
-            //    this onClick is to generate a random number between 18 and 100
-            $('#ranCompNum').on('click', function(){
+// create a click event for btns
+$(".button").on("click", function(){
+    console.log("clicked");
+    $("#message").text("");
+    var selectedID =$(this).attr("id");
+    // add valut to total
+    // check total
+    switch(selectedID){
+        case "btn1":
+            data.total = data.total + data.gems[0];
+            $("#total").text(data.total);
+            if(data.total> data.target){
+                handleLosses();
+            }else if(data.total === data.target){
+                handleWins();
+            }
+        break;
+        case "btn2":
+        data.total = data.total + data.gems[1];
+        $("#total").text(data.total);
+        if(data.total> data.target){
+            handleLosses();
+        }else if(data.total === data.target){
+            handleWins();
+        }
+    break;
+    case "btn3":
+    data.total = data.total + data.gems[2];
+    $("#total").text(data.total);
+    if(data.total> data.target){
+        handleLosses();
+    }else if(data.total === data.target){
+        handleWins();
+    }
+break;
+case "btn4":
+data.total = data.total + data.gems[3];
+$("#total").text(data.total);
+if(data.total> data.target){
+    handleLosses();
+}else if(data.total === data.target){
+    handleWins();
+}
+break;
 
-                     var generateComputerChoice = [];
-                document.getElementById("ranCompNum").innerHTML=choiceComp;
-               
-              
-            });
-            // $('#mydiv').attr("data-myval","20"); //setter
-        // alert($('#outer').html());
+    }
+});
 
-               
-
-            // $( '.crystals').on('click', function(){
-            //     var generateUserChoice = ""
-            //     var choice1 = Math.floor(Math.random()*12) +1;  
-            //     generateUserChoice = generateUserChoice + choice1
-            //     $('.crys1').append('<p>'+ generateUserChoice + '</p>')
-            // })
-                
-         
+function handleLosses(){
+    data.losses++;
+    $("#message").text("you lose!");
+    resetGame();
+}
+function handleWins(){
+    data.wins++;
+    $("#message").text("you win!");
+    resetGame();
+}
+function resetGame(){
+    data.target =  numberGenerator(19, 120);
+    data.total = 0;
+    data.gems= [];
+    assignGemValues();
+    displayHTML();
+}
